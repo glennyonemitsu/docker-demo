f834da600529f6e3b4c2d52d9f10c2339b2171b9
Glenn Yonemitsu <yonemitsu@gmail.com>
2016-04-11 15:52:25 -0400

    port encoding for hello world

--- 2/server.py
+++ 2/server.py
@@ -15,7 +15,7 @@ class HelloWorldHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200, 'OK')
         self.end_headers()
-        self.wfile.write(b'Hello, World, from port {}'.format(port))
+        self.wfile.write('Hello, World, from port {}'.format(port).encode('ascii'))
 
 
 server = HTTPServer(('', port), HelloWorldHandler)
